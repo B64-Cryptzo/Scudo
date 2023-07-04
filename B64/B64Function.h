32de7128c70fd1ee84f5646f37352a834551646a
Enzo Genovese <135083883+B64-Cryptzo@users.noreply.github.com>
2023-07-04 00:36:53 -0700

    Update B64Function.h

--- B64/B64Function.h
+++ B64/B64Function.h
@@ -2,27 +2,30 @@
 #include <windows.h>
 #include "../B64/B64Opcode.h"
 
-
 std::int32_t GetFunctionLength(void* function) {
 
 	if (!function)
 		return 0;
 
-	PCHAR functionAddress = PCHAR(function); //Address to start
-	unsigned long functionSize = 0ul; //Function Size
+	// Start Address
+	PCHAR functionAddress = PCHAR(function);
 
-	// Make sure this is actually a valid address
+	// Our return variable
+	std::int32_t functionLength = 0;
+
+	// Loop untill we reach double int3 breakpoints
 	while (functionAddress && *PWORD(functionAddress) != 0xCCCC) { //while valid address and value at address isnt breakpoint
-		// Calculate the size of the instruction
-
 		
+		// Get the size of the instruction
 		size_t instructionSize = OpDisassemble(functionAddress).GetLength();
-		// Size of INT3 is 1 byte. If the following byte is also 0xCC with a size of 1 also, chances are, we've reached the end of the function.
-		if (instructionSize == 1 && OpDisassemble(functionAddress + 1).GetLength() == 1 && (*PWORD(functionAddress) == 0xCCCC))
+
+		// Check if double int3 is two seperate instructions
+		if (instructionSize == 0x1 && OpDisassemble(functionAddress + 1).GetLength() == 0x1 && (*PWORD(functionAddress) == 0xCCCC))
 			break;
-		// Iterate past
-		functionSize += instructionSize;
+		
+		// Increment to the next instruction
+		functionLength += instructionSize;
 		functionAddress += instructionSize;
 	}
-	return functionSize;
+	return functionLength;
 }
